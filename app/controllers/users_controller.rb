0c9cdbb825a5b64d36cb6c583e957c8c5cbfcf0f
Jessica <jessicajoly88@gmail.com>
2015-10-28 15:47:25 -0700

    Food options display on use profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,28 @@ class UsersController < ApplicationController
 
   def show 
   	@user = User.find(params[:id])
+  		@foods = Food.all
   end 
+
+  def edit
+  	@user = User.find(params[:id])
+  	@foods = Food.all
+  end 
+
+  def update 
+  	@post = Post.find(params[:id])
+    food_ids = params[:user][:food]
+  	if food_ids.length > 0
+        food_ids.each do |food_id|
+            food = Tag.find(food_id)
+            @user.foods.push(tag)
+        end 
+      redirect_to user_path(current_user)
+  	else
+      render :edit
+    end
+  end
+
+   
+
 end
